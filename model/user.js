66413b117948f7d8ce646ff15f98c7e654dee9c1
tweakxr <95289066+tweakxr@users.noreply.github.com>
2022-02-15 12:52:44 +0300

    Create user.js

---
+++ model/user.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+
+const UserSchema = new mongoose.Schema({
+    username: {type: String, unique: true, required: true},
+    password: {type: String, required: true},
+}, {collection: 'users'})
+
+const model = mongoose.model('UserSchema', UserSchema)
+module.exports = model
